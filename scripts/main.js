0e9667a32802c5e358280b7dc4c3a4347a845ca7
Egor Ondatrov <realitzegor@gmail.com>
2023-12-15 17:01:31 +0200

    slider day2

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".slider", {
+  slidesPerView: "auto",
+  // centeredSlides: true,
+  spaceBetween: 25,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
