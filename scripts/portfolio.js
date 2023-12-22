c0af310ea9b983efb0d9d0c0d7502943cd9450a3
Egor Ondatrov <realitzegor@gmail.com>
2023-12-22 16:30:13 +0200

    portfolio done

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -6,13 +6,19 @@ list = [
     item: "Все работы",
     posts: [
       {
-        date: "17 о234rктября",
+        date: "17 октября",
         inDev: true,
         heading: "IT Profy: Консалтинг /Создание дизайна / Вёрстка ",
         body: "In today's digital era, Facebook Ads are an indispensable part of your marketing strategy. These ads precisely target and reach audiences by demographics, interests, and prior engagement with your content.",
       },
       {
-        date: "17 о234rктября",
+        date: "17 октября",
+        inDev: true,
+        heading: "IT Profy: Консалтинг /Создание дизайна / Вёрстка ",
+        body: "In today's digital era, Facebook Ads are an indispensable part of your marketing strategy. These ads precisely target and reach audiences by demographics, interests, and prior engagement with your content.",
+      },
+      {
+        date: "17 октября",
         inDev: true,
         heading: "IT Profy: Консалтинг /Создание дизайна / Вёрстка ",
         body: "In today's digital era, Facebook Ads are an indispensable part of your marketing strategy. These ads precisely target and reach audiences by demographics, interests, and prior engagement with your content.",
@@ -89,14 +95,30 @@ showPosts = (index) => {
   portfolio_item_posts.innerHTML = "";
   list[index].posts.map(({ date, inDev, heading, body }) => {
     portfolio_item_posts.innerHTML += `
-      <p class="p2">${date}</p>
-      <h5>${heading}</h5>
-      <p class="p1">${body}</p>
+    <li class="portfolio-post">
+      <div class="post-info">
+        <div class="post-meta">
+          <time datetime="2023-10-17" class="p2"
+          style="border-radius: 100px;
+                 border: 1px solid var(--Grey);
+                 padding: 8px 16px"
+          >${date}</time>
+          ${
+            inDev
+              ? `<p class="p2" style="color: var(--Red); padding: 8px 16px">В разработке</p>`
+              : ""
+          }
+        </div>
+        <h5>${heading}</h5>
+        <p class="p1">${body}</p>
+      </div>
+      <img src="./images/portfolio/item${1}/post${1}.png" class="post-img"/>
+    </li>
     `;
   });
 };
 
-showPosts(1);
+showPosts(0);
 
 activateBtn = (_index) => {
   for (let i = 0; i < portfolio_ul.children.length; i++) {
